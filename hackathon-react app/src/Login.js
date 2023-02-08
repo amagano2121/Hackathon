9e64e971d2cd1871dec6ae9af2ff8e119af6d110
Ana <amagano@travelers.com>
2023-02-08 14:59:58 -0500

    Login changed

--- hackathon-react app/src/Login.js
+++ hackathon-react app/src/Login.js
@@ -1,1 +1,13 @@
-//This is where the login will be 
+//This is where the login will be
+
+render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={this.state.value} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
