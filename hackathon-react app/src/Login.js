d2f45b8c9d34284c417b4e06ddc9f1cc0c6d1035
Ana <amagano@travelers.com>
2023-02-08 16:19:13 -0500

    Update

--- hackathon-react app/src/Login.js
+++ hackathon-react app/src/Login.js
@@ -1,18 +1,32 @@
 
-render(); {
+export const Login = () => {
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <label for="username">
           Username:
         </ label>
+        
+        <input type="text" id="username" />
 
         <label for="password">
           Password:
-        </ label> 
-        
-        <input type="text" value={this.state.value} />
+        </ label>  
+
+        <input type="text" id="password" />
 
         <input type="submit" value="Submit" />
       </form>
     );
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const usernameInput = event.target.username;
+    const passwordInput = event.target.password;
+
+    console.log(usernameInput.value);
+    console.log(passwordInput.value);
+  }
+
+  
